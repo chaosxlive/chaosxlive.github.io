929bb8433f6dab421bacad247cb821e208d80cc4
chaosxlive <chaosxlive@gmail.com>
2020-06-26 06:33:14 +0800

    新增：常用文件連結。 新增：Math Game pages。 新增：Github source link。

---
+++ documents.js
@@ -0,0 +1,25 @@
+// var document_links = [
+//     [
+//         name: 'Bootstrap',
+//         version: '4.0',
+//         href: 'https://getbootstrap.com/docs/4.0/getting-started/introduction/'
+//     ],
+//     {
+//         name: 'FontAwesome',
+//         version: '4.7.0',
+//         href: 'https://fontawesome.com/v4.7.0/icons/'
+//     }
+// ];
+
+var docs = {
+    0: {
+        name: 'Bootstrap',
+        version: '4.0',
+        href: 'https://getbootstrap.com/docs/4.0/getting-started/introduction/'
+    },
+    1: {
+        name: 'FontAwesome',
+        version: '4.7.0',
+        href: 'https://fontawesome.com/v4.7.0/icons/'
+    }
+};
